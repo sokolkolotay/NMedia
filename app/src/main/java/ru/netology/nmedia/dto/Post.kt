2e846c3925f04e5a923725b8fc96cc7cd4a7bde9
kolotayilya <ilha.kolotai@yandex.ru>
2025-05-29 23:27:39 +0700

    Added repainting of the like button by pressing and displaying numbers of different sizes

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,13 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Int,
+    val author: String,
+    val published: String,
+    val content: String,
+    var likes: Int = 999,
+    var comments: Int = 150_435,
+    var reposts: Int = 1_898,
+    var views: Int = 9_599_211,
+    var likeByMe: Boolean = false
+)
