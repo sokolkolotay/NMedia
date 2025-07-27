8b5d4730c34dc1ef601696f39114010ba514a644
kolotayilya <ilha.kolotai@yandex.ru>
2025-07-27 17:56:53 +0700

    Migrated to SQLite

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -11,31 +11,55 @@ import ru.netology.nmedia.activity.FeedFragment.Companion.textArgs
 import ru.netology.nmedia.databinding.FragmentNewPostBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
+
 class NewPostFragment : Fragment() {
+
+    private var bindingMain: FragmentNewPostBinding? = null
+    private val binding
+        get() = bindingMain!!
+
+    private val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentNewPostBinding.inflate(
+        bindingMain = FragmentNewPostBinding.inflate(
             inflater,
             container,
             false
         )
 
-        val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
-        arguments?.textArgs.let(binding.edit::setText)
+        //работа с черновиком
+        val draft = viewModel.draft
+        if (draft != null) {
+            binding.edit.setText(draft)
+        } else {
+            arguments?.textArgs?.let(binding.edit::setText)
+        }
 
         binding.save.setOnClickListener {
             if (binding.edit.text.isNotBlank()) {
                 val content = binding.edit.text.toString()
                 viewModel.changeContent(content)
                 viewModel.save()
+                viewModel.clearDraft()//очищаем черновик
             }
             findNavController().navigateUp()
         }
 
         return binding.root
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val content = binding.edit.text.toString()
+        viewModel.saveDraft(content)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bindingMain = null
     }
 }
